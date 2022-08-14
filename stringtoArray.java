d830653a3c6dddd963e83c58fcd7173c99c939b4
Navratna Dubey <106367427+Navratnadubey@users.noreply.github.com>
2022-08-15 00:53:58 +0530

    Add files via upload

---
+++ stringtoArray.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class stringtoArray {
+    public static void main(String[] args) {
+      String s="Hello";
+      char ch[]=s.toCharArray();
+      System.out.println(Arrays.toString(ch));
+  }
+}
